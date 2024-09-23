f667c32cf683ee2cdffad3a99102d2aaaa7e8a4f
Christian Camacho <chris.ca.dev@gmail.com>
2024-09-23 13:09:39 -0700

    refactor(config fzf) modified to always use native script

--- config/zsh/.oh-my-zsh/custom/fzf.zsh
+++ config/zsh/.oh-my-zsh/custom/fzf.zsh
@@ -2,11 +2,4 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 setopt appendhistory
-case $(~/projects/tech-workspace/init/commons/get-os-name.sh) in
-    "mac")
-        source <(fzf --zsh)
-    ;;
-    "debian" | "wsl-debian")
-        [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-    ;;
-esac
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
