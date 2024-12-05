990ea4d28f0e98a620c1a357df8c55226788726b
Christian Camacho <chris.ca.dev@gmail.com>
2024-12-05 08:04:24 -0800

    feat(config) modify to switch between pnpm directy location

--- config/nodejs/pnpm/.oh-my-zsh/custom/pnpm.zsh
+++ config/nodejs/pnpm/.oh-my-zsh/custom/pnpm.zsh
@@ -1,4 +1,9 @@
-export PNPM_HOME="/Users/ccamacho/Library/pnpm"
+OS="$(os-name)"
+if [[ $OS == "mac" ]]; then
+  export PNPM_HOME="~/Library/pnpm"
+elif [[ $OS == "ubuntu" ]]
+  export PNPM_HOME="~/.pnpm"
+fi
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
